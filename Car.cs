afc9bcb85328e542154307df1f37be8debf69f95
mein9 <mateo0221@gmail.com>
2024-07-27 10:15:35 -0700

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace AppQuiz02
+{
+    internal class Car
+    {
+        private string vin;
+        private string make;
+        private string model;
+        private int mileage;
+
+        public Car(string vin, string make, string model, int mileage)
+        {
+            this.vin = vin;
+            this.make = make;
+            this.model = model;
+            this.mileage = mileage;
+        }
+        public Car(string vin, string make, string model)
+        {
+            this.vin = vin;
+            this.make = make;
+            this.model = model;
+            this.mileage = 0;
+        }
+        public void Drive(int miles)
+        {
+            if (miles > 0)
+            {
+                mileage += miles;
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine($"VIN: {vin}");
+            Console.WriteLine($"Make: {make}");
+            Console.WriteLine($"Model: {model}");
+            Console.WriteLine($"Mileage: {mileage}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AppQuiz02;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create two Car objects using different constructors
+        Car car1 = new Car("1HGCM82633A123456", "Honda", "Accord", 50000);
+        Car car2 = new Car("1FTFW1ET4EKF12345", "Ford", "F-150");
+
+        // Drive the cars for different number of miles
+        car1.Drive(150);
+        car2.Drive(300);
+
+        // Display the details of each car
+        car1.Display();
+        car2.Display();
+    }
+}
